efd2008b36929e6917309beebc38464bbbab80a5
Pedro Mendes <106615792+pedroMendes182@users.noreply.github.com>
2023-03-29 16:14:44 -0300

    feat: add get contact per id route

--- src/controllers/contacts.controller.ts
+++ src/controllers/contacts.controller.ts
@@ -1,9 +1,15 @@
 import { Request, Response } from "express";
 import createContactService from "../services/contacts/createContact.service";
 import deleteContactService from "../services/contacts/deleteContact.service";
+import showContactService from "../services/contacts/showContact.service";
 
 export const createContactController = async (req: Request, res: Response) => {
   const data = await createContactService(req.user, req.body);
+  return res.status(200).json(data);
+};
+
+export const showContactController = async (req: Request, res: Response) => {
+  const data = await showContactService(req.params.id);
   return res.status(200).json(data);
 };
 
--- src/routes/contacts.routes.ts
+++ src/routes/contacts.routes.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import {
   createContactController,
   deleteContactController,
+  showContactController,
 } from "../controllers/contacts.controller";
 import ensureAuthMiddleware from "../middlewares/ensureAuth.middleware";
 import ensureContactExistsMiddleware from "../middlewares/ensureContactExists.middleware";
 import validateSchemasMiddleware from "../middlewares/validateData.middleware";
 import verifyAlreadyContactExistsMiddleware from "../middlewares/verifyAlreadyContactExists.middleware";
+import verifyUserTokenOwnerContactMiddleware from "../middlewares/verifyUserTokenIsOwnerContact.middleware";
 import { createContactSchema } from "../schemas/contacts.shema";
 
 const contactsRoutes = Router();
@@ -18,10 +20,18 @@ contactsRoutes.post(
   verifyAlreadyContactExistsMiddleware,
   createContactController
 );
+contactsRoutes.get(
+  "/:id",
+  ensureAuthMiddleware,
+  ensureContactExistsMiddleware,
+  verifyUserTokenOwnerContactMiddleware,
+  showContactController
+);
 contactsRoutes.delete(
   "/:id",
   ensureAuthMiddleware,
   ensureContactExistsMiddleware,
+  verifyUserTokenOwnerContactMiddleware,
   deleteContactController
 );
 
